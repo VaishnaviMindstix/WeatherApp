8e587e57f41a17c20a51f68c173ab17711f55a43
VaishnaviMindstix <vaishnavi.deshmukh@mindstix.com>
2025-05-09 13:33:46 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vaishnavi Deshmukh on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
